e06662e92b4be9cc572fc141ad5c1ac713548310
Eduardo433781 <eduardo.coerse@gmail.com>
2025-03-15 10:42:30 -0300

    feat: Atividades Java

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Média de três números
+        int num01 = 10;
+        int num02 = 20;
+        int num03 = 30;
+        int media= (num01+num02+num03)/3;
+
+        double media01 = (num01+num02+num03)/3.0;
+        float media02 = (num01+num02+num03)/3.0f;
+        long media03 = (num01+num02+num03)/3L;
+
+        System.out.println("A média do inteiro é: "+ media);
+        System.out.println("A média do número real é: "+ media01);
+        System.out.println("A média do número float é:"+ media02);
+        System.out.println("A média do número long é"+ media03);
+
+
+
+
+
+
+
+
+        }
+    }
